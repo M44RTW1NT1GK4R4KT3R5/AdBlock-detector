2b8a57ae09218c5663cbada60705808a52dedbfb
patrick ramge <patrick ramge>
2016-04-05 09:56:37 +0200

    Use this template to detect adblock users, and blockt them

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AdBlock Resistant webtemplate</title>
+		<script src="js/ads.js" type="text/javascript"/>
+	</head>
+	<body>
+		<script>
+			if( window.canRunAds === undefined ){
+        		// adblocker detected, place here the content
+    		}
+		</script>
+	</body>
+</html>
